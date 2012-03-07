4277689111f1bc87217cfcdd95b4c1f64bac77f8
KTamas <ktamas@ktamas.com>
2012-03-07 14:01:09 +0100

    fixed dumping

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,9 @@ module Planet
     config.middleware.use Rack::Superfeedr, { :host => sf["host"], :login => sf["login"], :password => sf["password"], :format => "json", :async => "false" } do |superfeedr|
       Superfeedr = superfeedr
       Superfeedr.on_notification do |n|
-        File.new("/web/sharewood.hu/_dump/#{Digest::SHA1.hexdigest(n)}", 'w').write(n).close()
+        f = File.new("/web/sharewood.hu/_dump/#{Digest::SHA1.hexdigest(n)}", 'w')
+        f.write(n)
+        f.close()
       end
     end
   end
